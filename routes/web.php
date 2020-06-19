d83fcab6849e5e5ce46120c6eae7956f2cbac5fc
Gabriel Delfino <gabrieldelfino28@gmail.com>
2020-06-18 21:40:51 -0300

    MEAJUDA

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,28 @@
 <?php
 
+use Illuminate\Support\Facades\Route; use App\User;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| 
+| adicionar rotas no server
+| 
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () { return view('welcome'); });
+
+Route::get('/userlist', 
+function () {
+for($contador = 0; $contador < 50; $contador++){
+$umUsuarioQualquer = new User;
+$umUsuarioQualquer->name = 'umUser'.$contador;
+$timestamp = date("Y-m-d-h-i-sa");
+$umUsuarioQualquer->email = "email".$contador."@emailTeste.com".$timestamp;
+$umUsuarioQualquer->password = '123';
+$umUsuarioQualquer->save(); }
+return User::all(); 
 });
